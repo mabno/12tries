43f7c954429fb058dc9b7250ec6792c197e5d4b3
Mariano Baldovino <mariano1baldovino@gmail.com>
2025-11-17 14:54:30 +0100

    feat: add viewport meta tag for responsive design

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -85,6 +85,9 @@ export default async function RootLayout({ children, params: { locale } }: { chi
 
   return (
     <html lang={locale}>
+      <head>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
+      </head>
       <body className={inter.className}>
         <Providers>
           <NextIntlClientProvider messages={messages}>
